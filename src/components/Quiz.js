8d529094392008145763259292879c224edcfff9
JEYASEELAN-2399 <115181970+JEYASEELAN-2399@users.noreply.github.com>
2023-12-26 07:44:43 +0530

    react-quiz app

---
+++ src/components/Quiz.js
@@ -0,0 +1,13 @@
+import Question from "./Question";
+
+const Quiz = () => {
+  return (
+    <div className="quiz">
+      <div className="score">Question 1/8</div>
+      <Question />
+      <div className="next-button">NEXT QUESTION</div>
+    </div>
+  );
+};
+
+export default Quiz;
